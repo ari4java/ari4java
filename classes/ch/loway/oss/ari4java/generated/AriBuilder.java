2a4b821d124fcdd0fa720b0638cbcee58455b267
Lenz E <lenz73@gmail.com>
2013-12-30 20:48:38 +0100

    AriBuilder methods - autogenerated - ver 0.1.1

---
+++ classes/ch/loway/oss/ari4java/generated/AriBuilder.java
@@ -0,0 +1,70 @@
+package ch.loway.oss.ari4java.generated;
+
+// ----------------------------------------------------
+//      THIS CLASS WAS GENERATED AUTOMATICALLY         
+//               PLEASE DO NOT EDIT                    
+// ----------------------------------------------------
+
+
+public interface AriBuilder {
+    public abstract ActionApplications actionApplications();
+    public abstract ActionAsterisk actionAsterisk();
+    public abstract ActionBridges actionBridges();
+    public abstract ActionChannels actionChannels();
+    public abstract ActionDeviceStates actionDeviceStates();
+    public abstract ActionEndpoints actionEndpoints();
+    public abstract ActionEvents actionEvents();
+    public abstract ActionPlaybacks actionPlaybacks();
+    public abstract ActionRecordings actionRecordings();
+    public abstract ActionSounds actionSounds();
+    public abstract Application application();
+    public abstract ApplicationReplaced applicationReplaced();
+    public abstract AsteriskInfo asteriskInfo();
+    public abstract Bridge bridge();
+    public abstract BridgeCreated bridgeCreated();
+    public abstract BridgeDestroyed bridgeDestroyed();
+    public abstract BridgeMerged bridgeMerged();
+    public abstract BuildInfo buildInfo();
+    public abstract CallerID callerID();
+    public abstract Channel channel();
+    public abstract ChannelCallerId channelCallerId();
+    public abstract ChannelCreated channelCreated();
+    public abstract ChannelDestroyed channelDestroyed();
+    public abstract ChannelDialplan channelDialplan();
+    public abstract ChannelDtmfReceived channelDtmfReceived();
+    public abstract ChannelEnteredBridge channelEnteredBridge();
+    public abstract ChannelHangupRequest channelHangupRequest();
+    public abstract ChannelLeftBridge channelLeftBridge();
+    public abstract ChannelStateChange channelStateChange();
+    public abstract ChannelUserevent channelUserevent();
+    public abstract ChannelVarset channelVarset();
+    public abstract ConfigInfo configInfo();
+    public abstract DeviceState deviceState();
+    public abstract DeviceStateChanged deviceStateChanged();
+    public abstract Dial dial();
+    public abstract Dialed dialed();
+    public abstract DialplanCEP dialplanCEP();
+    public abstract Endpoint endpoint();
+    public abstract EndpointStateChange endpointStateChange();
+    public abstract Event event();
+    public abstract FormatLangPair formatLangPair();
+    public abstract LiveRecording liveRecording();
+    public abstract Message message();
+    public abstract MissingParams missingParams();
+    public abstract Playback playback();
+    public abstract PlaybackFinished playbackFinished();
+    public abstract PlaybackStarted playbackStarted();
+    public abstract RecordingFailed recordingFailed();
+    public abstract RecordingFinished recordingFinished();
+    public abstract RecordingStarted recordingStarted();
+    public abstract SetId setId();
+    public abstract Sound sound();
+    public abstract StasisEnd stasisEnd();
+    public abstract StasisStart stasisStart();
+    public abstract StatusInfo statusInfo();
+    public abstract StoredRecording storedRecording();
+    public abstract SystemInfo systemInfo();
+    public abstract Variable variable();
+
+}
+
