c0c8f862e3a8b475968ddc56d283574df920ce5b
Brett Tofel <brett.tofel@clearcaptions.com>
2017-12-20 14:08:06 -0500

    refactors rabbitmq integration test just assure basic client functionality test is set to @Ignore since it requires a rabbitmq server be running on localhost with default install

--- tests/ch/loway/oss/ari4java/clients/RabbitDirectIntegrationTest.java
+++ tests/ch/loway/oss/ari4java/clients/RabbitDirectIntegrationTest.java
@@ -1,6 +1,6 @@
 package ch.loway.oss.ari4java.clients;
 
-import ch.loway.oss.ari4java.ARIProxyManager;
+import ch.loway.oss.ari4java.tools.amqp.RabbitMQClient;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.junit.*;
@@ -14,12 +14,13 @@ import static org.junit.Assert.assertEquals;
  * Test AMQP functionality as integration test against localhost rabbitmq server
  *
  */
-public class AMQP_Test {
+public class RabbitDirectIntegrationTest {
     private static final String RABBITMQ_HOST = "localhost";
     private static final String VIRTUAL_HOST = "/";
+    private static final String QUEUE_NAME = "test_queue";
     private static final String EXCHANGE_TOPIC = "test_topic";
     private Connection connection;
-    private ARIProxyManager ariProxyManager;
+    private RabbitMQClient rabbitMQClient;
 
     @Before
     public void setUp() throws IOException, TimeoutException {
@@ -27,7 +28,7 @@ public class AMQP_Test {
         factory.setHost(RABBITMQ_HOST); // rabbitmq running on localhost can be used for AMQP integration testing
         factory.setVirtualHost(VIRTUAL_HOST);
         connection = factory.newConnection();
-        ariProxyManager = new ARIProxyManager(connection, EXCHANGE_TOPIC);
+        rabbitMQClient = new RabbitMQClient(QUEUE_NAME, EXCHANGE_TOPIC, connection);
     }
 
     @After
@@ -35,11 +36,16 @@ public class AMQP_Test {
         connection.close();
     }
 
-    @Test
+    /**
+     *  You should keep this integration test as @Ignore unless you run a rabbitmq server on localhost with default install
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Ignore
     public void sendAndRecieveMessage() throws IOException, InterruptedException {
         String message = "Hello World";
-        ariProxyManager.send(message);
-        String receivedMessage = ariProxyManager.receive();
+        rabbitMQClient.send(message);
+        String receivedMessage = rabbitMQClient.receive();
         assertEquals(message, receivedMessage);
     }
 }
