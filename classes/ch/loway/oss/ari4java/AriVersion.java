dc4ac0901d678ce0ff92a5bf981475e663769a89
sere <stefan.repke@elara-gmbh.de>
2019-02-05 14:54:05 +0100

    ARI_4_0_0

--- classes/ch/loway/oss/ari4java/AriVersion.java
+++ classes/ch/loway/oss/ari4java/AriVersion.java
@@ -12,7 +12,7 @@ import ch.loway.oss.ari4java.generated.ari_1_8_0.AriBuilder_impl_ari_1_8_0;
 import ch.loway.oss.ari4java.generated.ari_1_9_0.AriBuilder_impl_ari_1_9_0;
 import ch.loway.oss.ari4java.generated.ari_2_0_0.AriBuilder_impl_ari_2_0_0;
 import ch.loway.oss.ari4java.generated.ari_3_0_0.AriBuilder_impl_ari_3_0_0;
-
+import ch.loway.oss.ari4java.generated.ari_4_0_0.AriBuilder_impl_ari_4_0_0;
 import ch.loway.oss.ari4java.tools.ARIException;
 
 /**
@@ -42,8 +42,10 @@ public enum AriVersion {
     /** Asterisk 14.2.1 */
     ARI_2_0_0 ( "2.0.0", new AriBuilder_impl_ari_2_0_0() ),
     /** Asterisk 15.1.4 */
-    ARI_3_0_0 ( "3.0.0", new AriBuilder_impl_ari_3_0_0() ),   /** Asterisk 15.1.4 */
-    
+    ARI_3_0_0 ( "3.0.0", new AriBuilder_impl_ari_3_0_0() ),
+    /** Asterisk 16.1.1 */
+    ARI_4_0_0 ( "4.0.0", new AriBuilder_impl_ari_4_0_0() ),
+
     
     IM_FEELING_LUCKY ( "", null );
 
--- codegen/ch/loway/oss/ari4java/codegen/run.java
+++ codegen/ch/loway/oss/ari4java/codegen/run.java
@@ -39,7 +39,8 @@ public class run {
         loadAsteriskDefs( dm, "ari_1_10_0" );
         loadAsteriskDefs( dm, "ari_2_0_0" );
         loadAsteriskDefs( dm, "ari_3_0_0" );
-        
+        loadAsteriskDefs( dm, "ari_4_0_0" );
+
         dm.generateAllClasses();
 
 
