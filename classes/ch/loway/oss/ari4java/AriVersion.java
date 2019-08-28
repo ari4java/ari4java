dcc3550fdcc2de29e56cbe4f6e8a60650b314bfa
neimaravila <neimar.avila@gmail.com>
2019-08-28 07:50:22 -0300

    Asterisk 16.5.0

--- classes/ch/loway/oss/ari4java/AriVersion.java
+++ classes/ch/loway/oss/ari4java/AriVersion.java
@@ -13,6 +13,7 @@ import ch.loway.oss.ari4java.generated.ari_1_9_0.AriBuilder_impl_ari_1_9_0;
 import ch.loway.oss.ari4java.generated.ari_2_0_0.AriBuilder_impl_ari_2_0_0;
 import ch.loway.oss.ari4java.generated.ari_3_0_0.AriBuilder_impl_ari_3_0_0;
 import ch.loway.oss.ari4java.generated.ari_4_0_0.AriBuilder_impl_ari_4_0_0;
+import ch.loway.oss.ari4java.generated.ari_4_0_2.AriBuilder_impl_ari_4_0_2;
 import ch.loway.oss.ari4java.tools.ARIException;
 
 /**
@@ -45,6 +46,8 @@ public enum AriVersion {
     ARI_3_0_0 ( "3.0.0", new AriBuilder_impl_ari_3_0_0() ),
     /** Asterisk 16.1.1 */
     ARI_4_0_0 ( "4.0.0", new AriBuilder_impl_ari_4_0_0() ),
+     /** Asterisk 16.5.0 */
+    ARI_4_0_0 ( "4.0.2", new AriBuilder_impl_ari_4_0_2() ),
 
     
     IM_FEELING_LUCKY ( "", null );
