bdbb149c7dbb85721d2c8308732ca2e6965887a6
Graham Brown <grahambrown11@gmail.com>
2020-02-19 08:18:39 +0200

    add logger and check content length

--- src/main/java/ch/loway/oss/ari4java/tools/http/NettyHttpClientHandler.java
+++ src/main/java/ch/loway/oss/ari4java/tools/http/NettyHttpClientHandler.java
@@ -1,12 +1,16 @@
 package ch.loway.oss.ari4java.tools.http;
 
 import ch.loway.oss.ari4java.tools.ARIEncoder;
+import ch.loway.oss.ari4java.tools.RestException;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.timeout.ReadTimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
@@ -24,6 +28,8 @@ public class NettyHttpClientHandler extends SimpleChannelInboundHandler<Object> 
     protected HttpResponseStatus responseStatus;
     private Throwable exception;
 
+    private Logger logger = LoggerFactory.getLogger(NettyHttpClientHandler.class);
+
     public void reset() {
         responseBytes = null;
         responseStatus = null;
@@ -33,14 +39,21 @@ public class NettyHttpClientHandler extends SimpleChannelInboundHandler<Object> 
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (msg instanceof FullHttpResponse) {
             FullHttpResponse response = (FullHttpResponse) msg;
+            if (logger.isTraceEnabled()) {
+                logger.trace("Headers: {}", response.headers().toString());
+            }
             responseBytes = new byte[response.content().readableBytes()];
             response.content().readBytes(responseBytes);
             responseStatus = response.status();
-        } else {
-            // TODO: what?
-            if (msg != null) {
-                System.out.println("Unknown object:" + msg);
+            if (response.headers().get(HttpHeaderNames.CONTENT_LENGTH) != null) {
+                if (responseBytes.length != response.headers().getInt(HttpHeaderNames.CONTENT_LENGTH)) {
+                    logger.error("HTTP Content-Length: {}, but body read: {}",
+                            response.headers().getInt(HttpHeaderNames.CONTENT_LENGTH), responseBytes.length);
+                }
             }
+        } else if (msg != null) {
+            logger.warn("Unexpected: {}", msg.toString());
+            throw new RestException("Unknown object: " + msg.getClass().getSimpleName() + ", expecting FullHttpResponse");
         }
     }
 
@@ -70,7 +83,7 @@ public class NettyHttpClientHandler extends SimpleChannelInboundHandler<Object> 
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         this.exception = cause;
         if (!(cause instanceof ReadTimeoutException)) {
-            cause.printStackTrace();
+            logger.error("Not a read timeout", cause);
         }
         ctx.fireExceptionCaught(cause);
         ctx.close();
